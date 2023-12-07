4dea9453a49b564916d002f8942477872b464709
DESKTOP-OFMMG60\HiKami <hikami172@gmail.com>
2023-12-07 04:33:41 +0100

    Completed introductory part

---
+++ src/main/java/a_Introductory/Fibonacci.java
@@ -0,0 +1,11 @@
+package main.java.a_Introductory;
+
+public class Fibonacci {
+	public int fib(int n) {
+		return switch (n) {
+			case 0 -> 0;
+			case 1 -> 1;
+			default -> (fib(n - 1) + fib(n - 2));
+		};
+	}
+}
